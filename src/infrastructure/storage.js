74fd140e0abc3b9a5989f7c18085e73586913e47
Yoshiya Hinosawa <stibium121@gmail.com>
2014-08-30 22:45:27 +0100

    Placeholder implementation for infrastructure.storage

---
+++ src/infrastructure/storage.js
@@ -0,0 +1,17 @@
+
+window.infrastructure = window.infrastructure = {};
+
+window.infrastructure = (function (straw, Promise) {
+    'use strict';
+
+    var exports = {};
+
+    exports.set = function (key, value) {
+    };
+
+    exports.get = function (key, default) {
+    };
+
+    return exports;
+
+}(straw, Promise));
