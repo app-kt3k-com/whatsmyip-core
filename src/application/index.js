820174cba8fdb768b07071799f872a726b757ff7
Yoshiya Hinosawa <stibium121@gmail.com>
2014-09-28 16:58:02 +0100

    Bump to version v0.1.1

--- src/application/index.js
+++ src/application/index.js
@@ -22,7 +22,7 @@ window.page.index = (function (window, $, t10, infrastructure) {
     var exports = {};
     var index = exports;
 
-    index.version = 'v0.1.0';
+    index.version = 'v0.1.1';
 
     exports.doReview = function () {
 
